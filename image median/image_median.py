f6620b6d275836d12aaedf60004e13c641d527ef
fireflyes <kappakill@gmail.com>
2016-09-09 21:27:06 -0700

    second

--- image median/image_median.py
+++ image median/image_median.py
@@ -1,22 +1,34 @@
 from PIL import Image
-from statistics import median
+import statistics
+import time
 
-
+startTime = time.time()*1000.0
 im = []
 for i in range(1,10):
-    im.append(Image.open(i+".png"))
-# loop through all the pixles and gather the median for each pixle
-temp = []
-final
-newimage = Image()
-for i in range (im[0].size()[0]):
-    for j in range (im[0].size()[1]):
-        for i in range (0,9):
-            temp.append(im.getpixel(i,j))
-        final = median(temp)
-        #sort the pixles and find the median
-        temp.
-        #store the median into the newimage
-        #delete all the data in temp
+    im.append(Image.open(str(i)+".png"))
 
-#save newimage to 
+size = im[0].size
+width, height = im[0].size
+
+
+
+
+
+pixel_values = []
+for i in range(0,9):
+    pixel_values.append(list(im[i].getdata()))
+pixels = []
+newImagePixels =[]
+for x in range(0,len(pixel_values[0])-1):
+    for i in range (0,9):
+        pixels.append(pixel_values[i][x])
+    pixels.sort()
+    newImagePixels.append(statistics.median(pixels))
+    del pixels[:]
+
+myimage10 = Image.new(im[0].mode,im[0].size);
+myimage10.putdata(newImagePixels)
+finalTime = time.time() * 1000.0
+
+print(str(finalTime-startTime))
+myimage10.save("10.png","PNG")
